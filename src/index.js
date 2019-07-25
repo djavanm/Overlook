9f76a6847acee41531f17f1b06ae2f9a60a075cd
Djavan Munroe <djavanm24@gmail.com>
2019-07-25 15:45:57 -0600

    Add Promise.all to index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import $ from 'jquery';
 import './css/base.scss';
 import Hotel from '../src/Hotel.js';
+import Bookings from '../src/Bookings.js';
+import Customer from '../src/Customer.js';
 import DOMupdates from './DOMupdates';
 
-
 let hotel;
-
+Promise.all([
+  fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users').then(response => response.json()),
+  fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms').then(response => response.json()),
+  fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings').then(response => response.json()),
+  fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/room-services/roomServices').then(response => response.json()),
+]).then(data => hotel = new Hotel(data[0].users, data[1].rooms, data[2].bookings, data[3].roomServices))
 
 console.log('This is the JavaScript entry file - your code begins here.');
