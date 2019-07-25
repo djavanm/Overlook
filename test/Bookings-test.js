89cddd719835754a17d6d92ed87d1fdbc11c86e9
Djavan Munroe <djavanm24@gmail.com>
2019-07-25 15:35:58 -0600

    Update test files

---
+++ test/Bookings-test.js
@@ -0,0 +1,8 @@
+import chai from 'chai';
+const expect = chai.expect;
+
+describe('See if the tests are running', () => {
+  it('should return true for Bookings', () => {
+    expect(true).to.equal(true);
+  });
+});
---
+++ test/Customer-test.js
@@ -0,0 +1,8 @@
+import chai from 'chai';
+const expect = chai.expect;
+
+describe('See if the tests are running', () => {
+  it('should return true for Customer', () => {
+    expect(true).to.equal(true);
+  });
+});
