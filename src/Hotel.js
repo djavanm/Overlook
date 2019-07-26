e32f1927513ed9c8fcd784ddce88fb8f29a6e1a0
Djavan Munroe <djavanm24@gmail.com>
2019-07-26 11:53:28 -0600

    Add create customers method to hotel

--- src/Hotel.js
+++ src/Hotel.js
@@ -18,30 +18,27 @@ class Hotel {
     if (month.length < 2) month = '0' + month;
     if (day.length < 2) day = '0' + day;
     this.today = [year, month, day].join('/');
-  }
+  };
+
   createCustomers() {
     this.userData.forEach(user => {
       let userBookings = this.bookingData.filter(booking => {
         return user.id === booking.userID
       });
-      if (this.roomServiceData.find(roomService => user.id === roomService.userID)) {
-        let userRoomServices = this.roomServiceData.filter(roomService => {
-          return user.id === roomService.userID
-        });
-      } else {
-        let userRoomServices = [];
-      }
+      let userRoomServices = this.roomServiceData.filter(roomService => {
+        return user.id === roomService.userID
+      });
       let userRooms = this.roomData.filter(room => {
         let roomNumbers = userBookings.map(bookings => {
           return bookings.roomNumber
-        })
+          })
         return roomNumbers.includes(room.number)
       })
       let customer = new Customer(user.id, user.name, userBookings, userRoomServices, userRooms);
-      console.log(customer)
+
       this.customers.push(customer);
     })
-  }
+  };
 };
 
   export default Hotel;
