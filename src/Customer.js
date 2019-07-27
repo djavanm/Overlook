4a9be5abcaa2cf44051de5b46d768403c55470f3
Djavan Munroe <djavanm24@gmail.com>
2019-07-27 16:25:22 -0600

    Refactor calculate costs methods

--- src/Customer.js
+++ src/Customer.js
@@ -8,24 +8,34 @@ class Customer {
   }
 
   calculateBill(date) {
-    var costs = 0;
+    let costs = this.calculateRoomCost(date) + this.calculateRoomServiceCost(date);
+    return parseFloat(costs.toFixed(2));
+  }
+
+  calculateRoomCost(date) {
     if(this.bookings.find(booking => booking.date === date)) {
-    var roomNum = this.bookings.find(booking => booking.date === date).roomNumber;
-    var roomCost = this.rooms.find(room => room.number === roomNum).costPerNight;
-    costs += roomCost;
+      let roomNum = this.bookings.find(booking => booking.date === date).roomNumber;
+      let costs = this.rooms.find(room => room.number === roomNum).costPerNight;
+    return parseFloat(costs.toFixed(2));
+    } else {
+      return 0;
     }
+  };
+
+  calculateRoomServiceCost(date) {
     if(this.roomService.find(order => order.date === date)) {
-      var roomServiceCost = this.roomService.reduce((totalCost, order) => {
+      let costs = this.roomService.reduce((totalCost, order) => {
         if(order.date === date) {
           totalCost += order.totalCost;
         }
         return totalCost;
       }, 0)
-      costs += roomServiceCost
-    }
-    return parseFloat(costs.toFixed(2));
+      return parseFloat(costs.toFixed(2));
+      } else {
+        return 0;
+      }
   }
-
 }
+
 
 export default Customer;
