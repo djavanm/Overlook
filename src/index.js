4cf50974490e6d2abac0a27391cf2ef819d5ae73
Djavan Munroe <djavanm24@gmail.com>
2019-07-28 13:42:09 -0600

    Strt html sections

--- src/index.js
+++ src/index.js
@@ -12,5 +12,6 @@ Promise.all([
   fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings').then(response => response.json()),
   fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/room-services/roomServices').then(response => response.json()),
 ]).then(data => hotel = new Hotel(data[0].users, data[1].rooms, data[2].bookings, data[3].roomServices))
-  .then(data => hotel.open());
+  .then(data => hotel.open())
+  .then(data => console.log(hotel));
 
