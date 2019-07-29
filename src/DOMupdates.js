78d6dd28b65a829a20adcf59fe104496fa568e53
Djavan Munroe <djavanm24@gmail.com>
2019-07-28 18:44:46 -0600

    Add search to dom

--- src/DOMupdates.js
+++ src/DOMupdates.js
@@ -1,8 +1,16 @@
 import $ from 'jquery';
 
 const DOMupdates = {
-  sample() {
-    
+  displaySearchResults(matches) {
+    $('.search__list').empty();
+    if(matches.length > 0 && matches.length < 100) {
+      let searchHtml = matches.slice(0, 10).map(match => `
+      <article  class="search__customer">
+      <h4>${match.name}</h4>
+      </article>
+      `).join('');
+      $('.search__list').append(searchHtml);
+    }
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -16,4 +16,42 @@ Promise.all([
   .then(data => console.log(hotel));
 
 
+
+$(document).ready(() => {
+  $('.main__panel-container .main__panel-tabs li').on('click', function() {
+    $('.main__panel-container .main__panel-tabs li.active').removeClass('active'); 
+    $(this).addClass('active')
+    // figure out which panel to show
+    let panelToShow = $(this).attr('rel');
+    // hide current panel 
+    $('.main__panel-container .panel.active').slideUp(300, showNextPanel) 
+    // show new panel
+    function showNextPanel() {
+      $(this).removeClass('active');
+      $('#'+panelToShow).slideDown(200, function() {
+        $(this).addClass('active');
+      });
+    };
+  });
+
+  $('.header__search-input').on('keydown', customerSearch);
+
+  function customerSearch() {
+      let searchText = $('.header__search-input').val().toLowerCase();
+      let matches = hotel.customers.filter(customer => {
+        return customer.name.toLowerCase().includes(searchText);
+      })
+      if(searchText.length === 0) {
+        matches = [];
+        $('.search__list').empty();
+      }
+      DOMupdates.displaySearchResults(matches);
+  }
+
+  $('.search__list').on('click', function() {
+    console.log('hi');
+  })
+});
+
+
   
